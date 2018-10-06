3c82b8bb6d76d5196243f1e8e6fe6d084c402199
varun kumar <varunakaduke@gmail.com>
2018-10-06 15:03:35 +0530

    complete course project - the basic -3

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,10 +7,10 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a Recipe', 'https://www.google.co.in/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj4zoWqiOjdAhXLuY8KHUf2DdQQjRx6BAgBEAU&url=https%3A%2F%2Fcooking.nytimes.com%2Frecipes%2F1018068-chicken-paprikash&psig=AOvVaw1TNUVQ1UXXQigUQ6zya95r&ust=1538580279059669' )
+ recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a Recipe', 'https://images.media-allrecipes.com/userphotos/600x600/4573350.jpg' ),
+    new Recipe('A Test Recipe', 'This is a Recipe', 'https://images.media-allrecipes.com/userphotos/600x600/4573350.jpg' )
   ];
-
   constructor() { }
 
   ngOnInit() {
